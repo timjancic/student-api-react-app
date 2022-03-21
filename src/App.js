1c6ebad87a01b7c3bed72a2890b7d4d2363a48b2
timjancic <timjancic.gravity@gmail.com>
2022-03-21 15:44:27 -0500

    Fetch data on load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {}
+
+    let dataAPI;
+
+    //call the fetch function
+    fetch('https://api.hatchways.io/assessment/students')
+    .then(res => res.json())//response type
+    .then(data => {
+      //console.log(data);
+      dataAPI = data.students;
+      this.state.students = dataAPI;
+    });
+
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <p>Placeholder</p>
+      </div>
+    );
+  }
+
 }
 
 export default App;
